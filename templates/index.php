e63258d5244cbfa98667ed9456ea97138c8c263a
Christian Weiske <cweiske@cweiske.de>
2014-08-21 23:23:19 +0200

    use standard owncloud app layout

--- templates/index.php
+++ templates/index.php
@@ -1,7 +1,13 @@
-<div style="margin: 1em;">
- <h1 style="font-size: 2em; font-weight: bold">Tomboy notes server</h1>
- <p style="margin: 1em;">
-  To use the REST API, use the follwing server with tomboy:
- </p>
- <pre style="margin: 1em; background-color: #DDD; padding: 1ex; font-family: monospace"><?php p($_['apiurl']); ?></pre>
+<div id="app">
+  <div id="app-navigation">
+</div>
+
+<div id="app-content">
+  <div style="margin: 1ex">
+    <h1 style="font-size: 2em; font-weight: bold">Tomboy notes server</h1>
+    <p style="margin: 1em;">
+      Use the following sync server URL with tomboy/conboy/tomdroid:
+    </p>
+    <pre style="margin: 1em; background-color: #DDD; padding: 1ex; font-family: monospace"><?php p($_['apiurl']); ?></pre>
+  </div>
 </div>
