0b208d3ef2a8501713169b74eb0b48e2e3bb10a6
Christian Weiske <cweiske@cweiske.de>
2014-10-14 07:46:07 +0200

    Link pages from stats

--- templates/indexStats.php
+++ templates/indexStats.php
@@ -1,9 +1,9 @@
 <h2>Statistics</h2>
 <dl>
- <dt>Number of notes</dt>
+ <dt>Number of <a class="lined" href="<?php p(OCP\Util::linkToRoute('grauphel.gui.tag', array('rawtag' => 'grauphel:special:all'))); ?>">notes</a></dt>
  <dd><?php p($_['notes']); ?></dd>
 
- <dt>Number of registered apps</dt>
+ <dt>Number of <a class="lined" href="<?php p(OCP\Util::linkToRoute('grauphel.gui.tokens')); ?>">registered apps</a></dt>
  <dd><?php p($_['tokens']); ?></dd>
 
  <dt>Sync revision</dt>
