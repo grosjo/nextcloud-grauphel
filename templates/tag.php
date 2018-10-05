948b974fcb916a6b340e3d86fc83a0f4b833f1c3
Josh Panter <joshu@unfettered.net>
2018-10-05 12:37:29 -0400

    Update tag.php

--- templates/tag.php
+++ templates/tag.php
@@ -19,7 +19,7 @@
     <?php foreach ($_['notes'] as $note) { ?>
      <tr id="note-<?php p($note['guid']); ?>">
       <td>
-       <a class="cellclick" href="<?php p(OCP\Util::linkToRoute('grauphel.gui.note', array('guid' => $note['guid']))); ?>"><?php echo ($note['title']); ?></a>
+       <a class="cellclick" href="<?php p(OC::$server->getURLGenerator()->linkToRoute('grauphel.gui.note', array('guid' => $note['guid']))); ?>"><?php echo ($note['title']); ?></a>
       </td>
       <td style="color: <?php echo p($note['dateColor']); ?>">
        <?php p(\OCP\Util::formatDate(strtotime($note['last-change-date']))); ?>
