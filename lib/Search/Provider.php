c636d694d252b2787f4c8aad1595c9e4ccddb5ed
Robert McQueen <rob@endlessos.org>
2023-08-31 13:05:15 +0100

    Search: fix basic functionality
    Following https://github.com/grosjo/nextcloud-grauphel/issues/100 and
    https://docs.nextcloud.com/server/27/developer_manual/digging_deeper/search.html
    try to return valid search results using SearchResult and SearchResultEntry.
    Correct invalid references in getName() and getOrder(). Remove unused
    Notes class and tidy whitespace.

--- lib/Search/Provider.php
+++ lib/Search/Provider.php
@@ -4,7 +4,7 @@ declare(strict_types=1);
 
 namespace OCA\Grauphel\Search;
 
-use OCA\Grauphel\NoteStorage;
+use OCA\Grauphel\Storage\NoteStorage;
 use OCA\Grauphel\AppInfo\Application;
 
 use OCP\IL10N;
@@ -17,54 +17,62 @@ use OCP\Search\SearchResultEntry;
 
 class Provider implements IProvider
 {
-        private IL10N $il10;
-	private IURLGenerator $url;
+    private IL10N $l10n;
+    private IURLGenerator $url;
 
-	public function __construct(IL10N $l10n, IURLGenerator $urlGenerator)
-        {
-                $this->il10 = $l10n;
-                $this->url = $urlGenerator;
-        }
+    public function __construct(IL10N $l10n, IURLGenerator $urlGenerator)
+    {
+        $this->l10n = $l10n;
+        $this->url = $urlGenerator;
+    }
 
     public function getId(): string
-    {   
+    {
         return Application::APP_ID;
     }
 
     public function getName(): string
-    {   
-        return $this->lutil->l10n->t('Grauphel');
+    {
+        return $this->l10n->t('Grauphel');
     }
 
     public function getOrder(string $route, array $routeParameters): int
-    {   
+    {
         if (strpos($route, $this->getId() . '.') === 0) {
             return -1;
         }
 
-        return self::ORDER;
+        return 55;
     }
 
     public function search(IUser $user, ISearchQuery $query) : SearchResult
     {
-        $notes  = new NoteStorage($this->urlGen);
-        $notes->setUsername( $user->getUID());
-        
-	$qp = new QueryParser();
-        $rows = $notes->search($qp->parse($query));
+        $notes  = new NoteStorage($this->url);
+        $notes->setUsername($user->getUID());
+
+        $qp = new QueryParser();
+        $rows = $notes->search($qp->parse($query->getTerm()));
+
+        $icon = $this->url->imagePath($this->getID(), 'app.svg');
 
         $results = array();
-        foreach ($rows as $row) 
-	{
-            $res = new Note();
-            $res->id   = $row['note_guid'];
-            $res->name = htmlspecialchars_decode($row['note_title']);
-            $res->link = $this->url->linkToRoute(
-                'grauphel.gui.note', array('guid' => $row['note_guid'])
+        foreach ($rows as $row)
+        {
+            $res = new SearchResultEntry(
+                $icon,
+                htmlspecialchars_decode($row['note_title']),
+                '',
+                $this->url->linkToRoute(
+                    'grauphel.gui.note', array('guid' => $row['note_guid'])
+                )
             );
             $results[] = $res;
         }
-        return $results;
+
+        return SearchResult::complete(
+            $this->getName(),
+            $results
+        );
     }
 }
 ?>
